0a087bbed42d1d60e27a2a54180c007daba4a9e5
0xukhezo <ukhezo.web3@gmail.com>
2022-09-20 00:05:57 +0200

    Resolve problem with web3 library and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import React from "react";
+import Coins from "./Components/Coins/Coins";
+import Home from "./Components/Home/Home";
 import "./App.css";
+import CoinDetails from "./Components/CoinDetails/CoinDetails";
 
 function App() {
-  return <div className="App">BitDonut</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/coins" element={<Coins />}></Route>
+      <Route path="/:id" element={<CoinDetails />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
