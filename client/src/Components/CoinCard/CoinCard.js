31abd5f5597f61c9fdbde29aa454a0575e0ccdca
0xukhezo <ukhezo.web3@gmail.com>
2022-09-20 19:48:22 +0200

    Coin Card for the coin display

---
+++ client/src/Components/CoinCard/CoinCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function CoinCard(props) {
+  let coin = props.coin;
+  let btc = props.bitcoin[0];
+  return (
+    <Link to={`/${coin.id}`}>
+      <ul>
+        <li>
+          <img src={coin.image} />
+          <span>{coin.name}</span>
+        </li>
+        <li>{coin.current_price.toFixed(2)} $</li>
+        <li>{coin.high_24h.toFixed(2)} $</li>
+        <li>{coin.low_24h.toFixed(2)} $</li>
+        <li
+          style={{
+            color: coin.price_change_percentage_24h < 0 ? "#dc2626" : "#22c55e",
+          }}
+        >
+          {coin.price_change_24h.toFixed(2)}
+          {coin.price_change_percentage_24h > 0 ? " ↑" : " ↓"}
+        </li>
+        <li
+          style={{
+            color: coin.price_change_percentage_24h < 0 ? "#dc2626" : "#22c55e",
+          }}
+        >
+          {coin.price_change_percentage_24h > 0 && " +"}
+          {coin.price_change_percentage_24h.toFixed(2)}%
+        </li>
+
+        <li>
+          {(coin.current_price / btc.current_price).toFixed(6)}{" "}
+          {btc.symbol.toUpperCase()}
+        </li>
+      </ul>
+    </Link>
+  );
+}
+
+export default CoinCard;
--- client/src/Components/Coins/Coins.js
+++ client/src/Components/Coins/Coins.js
@@ -1,12 +1,130 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import CoinCard from "../CoinCard/CoinCard";
 
-function Coins(props) {
+function Coins({ coins }) {
+  let [open, setOpen] = useState(false);
+  let [shorts, setShort] = useState();
+  let [allCoins, setCoins] = useState(coins);
+  let [allCoinsCopy, setCoinsCopy] = useState(coins);
+  let [search, setSearch] = useState("");
+  window.scrollTo(0, 0);
+  let bitcoin = allCoins.filter((coin) => {
+    return coin.id === "bitcoin";
+  });
+
+  let closeSearch = () => {
+    setSearch("");
+  };
+
+  let openDisplay = () => {
+    setOpen(true);
+  };
+
+  let hiddeDisplay = () => {
+    setOpen(false);
+    closeSearch();
+  };
+
+  let getShort = (id) => {
+    setShort(id);
+  };
+
+  const sortOptions = [
+    { value: "1", label: "Más caro", checked: false },
+    { value: "0", label: "Más barato", checked: false },
+  ];
+
+  const handleShortFilterChange = (e) => {
+    let shortFilterID = e.currentTarget.id;
+    getShort(shortFilterID);
+  };
+
+  useEffect(() => {
+    let copy = [...allCoins];
+    if (search.length !== 0) {
+      copy = allCoins.filter((coin) =>
+        coin.name.toLowerCase().includes(search)
+      );
+    }
+    if (shorts === 1) {
+      copy = copy.sort((a, b) => {
+        if (a.current_price > b.current_price) {
+          return -1;
+        }
+        if (a.current_price < b.current_price) {
+          return 1;
+        }
+        return 0;
+      });
+    } else if (shorts === 0) {
+      copy = copy.sort((a, b) => {
+        if (a.current_price > b.current_price) {
+          return 1;
+        }
+        if (a.current_price < b.current_price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    setCoinsCopy(copy);
+  }, [shorts, search]);
+
   return (
-    <div id="coin-list">
-      {props.coins?.map((coin) => {
-        return <div key={coin.id}></div>;
-      })}
-    </div>
+    allCoins && (
+      <div>
+        {open === true ? (
+          <>
+            <div>
+              <button onClick={hiddeDisplay}>Ocultar</button>
+            </div>
+            <div>
+              <ul>
+                {sortOptions.map((option) => (
+                  <li>
+                    <label htmlFor={option.value}>
+                      <input
+                        onChange={handleShortFilterChange}
+                        id={option.value}
+                        name="radio-filter"
+                        defaultChecked={false}
+                        key={option.value}
+                        type="radio"
+                      />
+                      <span>{option.label}</span>
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </>
+        ) : (
+          <div>
+            <button onClick={openDisplay}>Filtros</button>
+          </div>
+        )}
+
+        <ul>
+          <li>Nombre</li>
+          <li>Precio </li>
+          <li>Más alto 24h </li>
+          <li>Más bajo 24h </li>
+          <li>Cambio en 24h </li>
+          <li>Porcentaje en 24h</li>
+          <li>Cambio con BTC</li>
+        </ul>
+
+        <div>
+          {allCoinsCopy?.map((coin) => {
+            return (
+              <div key={coin.id}>
+                <CoinCard coin={coin} bitcoin={bitcoin} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    )
   );
 }
 
