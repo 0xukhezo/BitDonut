4d26686b94c5cb691245594c0cbcf5ec6f9ddcf2
0xukhezo <ukhezo.web3@gmail.com>
2022-09-20 23:15:01 +0200

    Home page and images folder

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,7 +1,117 @@
 import React from "react";
+import NavBar from "../Layout/NavBar";
+import GitHub from "../../Images/GitHub-Mark-32px.png";
+import Logo from "../../Images/Logo.png";
+import { Link } from "react-router-dom";
 
-function Home() {
-  return <div>Home</div>;
+function Home(props) {
+  let coins = props.coins;
+  window.scrollTo(0, 0);
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <h1>
+          BITDONUT<span>PROTOCOL</span>
+        </h1>
+        <p>
+          Swap, earn, and build on the leading decentralized crypto trading
+          protocol.
+        </p>
+        <a href="https://github.com/0xukhezo">
+          <img src={GitHub}></img>
+        </a>
+      </div>
+      <div>
+        <div>
+          <h3>{coins.length}+</h3>
+          <p>Cryptomonedas en curso</p>
+        </div>
+        <div>
+          <h3>400+</h3>
+          <p>Capitalizacion de Mercado</p>
+        </div>
+        <div>
+          <h3>300+</h3>
+          <p>Integraciones en Mercado</p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h3>300+</h3>
+          <p>Integraciones en aplicaciones</p>
+        </div>
+        <div>
+          <h3>Una red creciente de DeFi Apps.</h3>
+          <p>
+            Desarrolladores, operadores y proveedores de liquidez participan
+            juntos en un mercado financiero abierto y accesible para todos.
+          </p>
+        </div>
+      </div>
+      <div>
+        <div>
+          <Link to="/coins">
+            <h3>Mercado</h3>
+            <p>
+              Accede a nuestro mercado de cryptomonedas donde podras disfrutar
+              de más de {coins.length} monedas con toda la información de estas
+              con un historico de más de 90 dás. Con un display en gráfico para
+              que el usuario pueda ver en todo momento el precio de la moneda.
+            </p>
+          </Link>
+        </div>
+        <div>
+          <img src={Logo}></img>
+          <h3>Solicite financiación del Programa de Subvenciones BitDonut.</h3>
+          <p>
+            Construya aplicaciones y herramientas Defi en el mayor proyecto de
+            criptografía en Ethereum. A su disposición para empezar dispone de
+            todas las guías de inicio rápido, documentación de protocolos y un
+            SDK de Javascript.
+          </p>
+        </div>
+        <div>
+          <Link to="/portfolio">
+            <h3>Portfolio</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </Link>
+        </div>
+        <div>
+          <Link to="/convert">
+            <h3>Conversor</h3>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              />
+            </svg>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
