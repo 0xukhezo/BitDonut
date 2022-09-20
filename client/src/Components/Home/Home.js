5a05ae08900ded182223d7efa8501c92d440f5fb
0xukhezo <ukhezo.web3@gmail.com>
2022-09-20 23:28:18 +0200

    Home page's style & app's style

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "../Layout/NavBar";
 import GitHub from "../../Images/GitHub-Mark-32px.png";
 import Logo from "../../Images/Logo.png";
+import "./Home.css";
 import { Link } from "react-router-dom";
 
 function Home(props) {
@@ -12,7 +13,7 @@ function Home(props) {
       <div>
         <NavBar />
       </div>
-      <div>
+      <div id="header-home">
         <h1>
           BITDONUT<span>PROTOCOL</span>
         </h1>
@@ -24,7 +25,7 @@ function Home(props) {
           <img src={GitHub}></img>
         </a>
       </div>
-      <div>
+      <div id="section-one-home">
         <div>
           <h3>{coins.length}+</h3>
           <p>Cryptomonedas en curso</p>
@@ -38,12 +39,12 @@ function Home(props) {
           <p>Integraciones en Mercado</p>
         </div>
       </div>
-      <div>
-        <div>
+      <div id="section-two-home">
+        <div className="container1">
           <h3>300+</h3>
           <p>Integraciones en aplicaciones</p>
         </div>
-        <div>
+        <div className="container2">
           <h3>Una red creciente de DeFi Apps.</h3>
           <p>
             Desarrolladores, operadores y proveedores de liquidez participan
@@ -51,8 +52,8 @@ function Home(props) {
           </p>
         </div>
       </div>
-      <div>
-        <div>
+      <div id="section-three-home">
+        <div className="text1">
           <Link to="/coins">
             <h3>Mercado</h3>
             <p>
@@ -63,8 +64,8 @@ function Home(props) {
             </p>
           </Link>
         </div>
-        <div>
-          <img src={Logo}></img>
+        <div className="text4">
+          <img src={Logo} id="Logo-img"></img>
           <h3>Solicite financiación del Programa de Subvenciones BitDonut.</h3>
           <p>
             Construya aplicaciones y herramientas Defi en el mayor proyecto de
@@ -73,7 +74,7 @@ function Home(props) {
             SDK de Javascript.
           </p>
         </div>
-        <div>
+        <div className="text2">
           <Link to="/portfolio">
             <h3>Portfolio</h3>
             <svg
@@ -81,6 +82,7 @@ function Home(props) {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
+              className="svg"
             >
               <path
                 stroke-linecap="round"
@@ -91,7 +93,7 @@ function Home(props) {
             </svg>
           </Link>
         </div>
-        <div>
+        <div className="text3">
           <Link to="/convert">
             <h3>Conversor</h3>
             <svg
@@ -99,6 +101,7 @@ function Home(props) {
               fill="none"
               viewBox="0 0 24 24"
               stroke="currentColor"
+              className="svg"
             >
               <path
                 stroke-linecap="round"
