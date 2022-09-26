f5c1d5eccc74c9c4d9b502c7d9287aecbe15379b
0xukhezo <ukhezo.web3@gmail.com>
2022-09-26 23:49:22 +0200

    Campaigns inicial display

--- client/src/Components/Campaigns/Campaigns.js
+++ client/src/Components/Campaigns/Campaigns.js
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import factory from "../../Web3/factory";
+import NavBar from "../Layout/NavBar";
+import Logging from "../Logging/Logging";
 
-function Campaigns() {
-  return <div>Campaigns</div>;
+function Campaings() {
+  const [campaings, setCampaings] = useState([]);
+  let [isLoadding, setIsLoadding] = useState(true);
+
+  async function fetchData() {
+    const campaingsFromFetch = await factory.methods
+      .getDeployedCampaing()
+      .call();
+
+    setCampaings(campaingsFromFetch);
+    setIsLoadding(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [campaings]);
+
+  return (
+    <>
+      {isLoadding ? (
+        <Logging />
+      ) : (
+        <>
+          <NavBar />
+          <div>
+            <div>
+              <h1>Campaings</h1>
+              <Link to={"/newCampaign"}>
+                <button>Create a new Campaing </button>
+              </Link>
+            </div>
+
+            <div>
+              {campaings.map((campaing) => {
+                return (
+                  <Link to={`${campaing}`}>
+                    <div>
+                      <p>Campaing Hash {campaing}</p>
+                    </div>
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      )}
+    </>
+  );
 }
 
-export default Campaigns;
+export default Campaings;
