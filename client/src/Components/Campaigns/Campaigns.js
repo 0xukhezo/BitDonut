711295637d0d0c422dd9712666ce351e62c1be39
0xukhezo <ukhezo.web3@gmail.com>
2022-09-27 18:05:02 +0200

    Campaigns style

--- client/src/Components/Campaigns/Campaigns.js
+++ client/src/Components/Campaigns/Campaigns.js
@@ -4,6 +4,8 @@ import factory from "../../Web3/factory";
 import NavBar from "../Layout/NavBar";
 import Logging from "../Logging/Logging";
 
+import "./Campaigns.css";
+
 function Campaings() {
   const [campaings, setCampaings] = useState([]);
   let [isLoadding, setIsLoadding] = useState(true);
@@ -32,18 +34,18 @@ function Campaings() {
       ) : (
         <>
           <NavBar />
-          <div>
-            <div>
-              <h1>Campaings</h1>
-              <Link to={"/newCampaign"}>
+          <div id="display-campaings">
+            <div id="header-campaings">
+              <h1 id="title-campaings">Campaings</h1>
+              <Link to={"/newCampaign"} id="btn-new-campaing">
                 <button>Create a new Campaing </button>
               </Link>
             </div>
 
-            <div>
+            <div id="wrapper">
               {campaings.map((campaing) => {
                 return (
-                  <Link to={`${campaing}`}>
+                  <Link to={`${campaing}`} id="section-campaigns">
                     <div>
                       <p>Campaing Hash {campaing}</p>
                     </div>
